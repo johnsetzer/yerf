0bb9fc326de04defd3e6dced2b2187385b59835e
John Setzer <jsetzer@yammer-inc.com>
2013-04-27 20:39:04 -0700

    Added normTime and tests for getTime

---
+++ tests/test_helpers.js
@@ -0,0 +1,99 @@
+// Mocks yerf().getTime()
+var getTimeSpy;
+var mockGetTime = function (/* nextTimes */) {
+  var nextTimes = Array.prototype.slice.call(arguments);
+  getTimeSpy = spyOn(yerf(), 'getTime').andCallFake(function () {
+    if (nextTimes.length > 0) {
+      var time = nextTimes.shift();
+      return time;
+    } else {
+      return -1111; // Error Code
+    }
+  });
+};
+
+// Mocks window.performance.now()
+var nowSpy;
+var mockNow = function (/* nextTimes */) {
+  
+  // Make a fake performance object if testing on an old browser
+  if(!window.performance) {
+    window.performance = {
+      timing: {}
+    , now: function () {}
+    , webkitGetEntries: function () {}
+    };
+  }
+
+  var nextTimes = Array.prototype.slice.call(arguments);
+  nowSpy = spyOn(window.performance, 'now').andCallFake(function () {
+    if (nextTimes.length > 0) {
+      var time = nextTimes.shift();
+      return time;
+    } else {
+      return -1111; // Error Code
+    }
+  });
+};
+
+// Mocks yerf().getTime()
+var dateSpy;
+var mockDate = function (/* nextTimes */) {
+  var nextTimes = Array.prototype.slice.call(arguments);
+  dateSpy = spyOn(window, 'Date').andCallFake(function () {
+    if (nextTimes.length > 0) {
+      var time = nextTimes.shift();
+      return { getTime: function () { return time; }};
+    } else {
+      return { getTime: function () { return -1111; }}; // Error Code
+    }
+  });
+};
+
+var expectSample = function (sample, key, startedAt, stoppedAt, delta) {
+  expect(sample.key).toBe(key);
+  expect(sample.startedAt).toBe(startedAt);
+  expect(sample.stoppedAt).toBe(stoppedAt);
+  expect(sample.delta).toBe(delta);
+};
+
+var expectOnError = function (sample, expectedErrorMsg) {
+  var sampleOnError = spyOn(sample, 'onError').andCallFake(function (error) {
+    expect(error.message).toBe(expectedErrorMsg);
+  });
+  return sampleOnError;
+}
+
+jasmine.Spy.prototype.restore = function() {
+  this.baseObj[this.methodName] = this.originalValue;
+};
+
+describe('mockGetTime', function () {
+  it('makes yerf().getTime() return the fake time arguments until there are none left', function () {
+    mockGetTime(100, 200);
+    expect(yerf().getTime()).toBe(100);
+    expect(yerf().getTime()).toBe(200);
+    expect(yerf().getTime()).toBe(-1111);
+    expect(getTimeSpy.calls.length).toBe(3);
+  });
+});
+
+describe('mockNow', function () {
+  it('makes performance.now() return the fake time arguments until there are none left', function () {
+    mockNow(100, 200);
+    expect(performance.now()).toBe(100);
+    expect(performance.now()).toBe(200);
+    expect(performance.now()).toBe(-1111);
+    expect(nowSpy.calls.length).toBe(3);
+  });
+});
+
+describe('mockDate', function () {
+  it('makes Date.getTime() return the fake time arguments until there are none left', function () {
+    mockDate(100, 200);
+    expect(new Date().getTime()).toBe(100);
+    expect(new Date().getTime()).toBe(200);
+    expect(new Date().getTime()).toBe(-1111);
+    expect(dateSpy.calls.length).toBe(3);
+  });
+});
--- tests/yerf_spec.js
+++ tests/yerf_spec.js
@@ -1,109 +1,178 @@
 var expectOnError = function (sample, expectedErrorMsg) {
-    var sampleOnError = spyOn(sample, 'onError').andCallFake(function (error) {
-      expect(error.message).toBe(expectedErrorMsg);
-    });
-    return sampleOnError;
-  }
+  var sampleOnError = spyOn(sample, 'onError').andCallFake(function (error) {
+    expect(error.message).toBe(expectedErrorMsg);
+  });
+  return sampleOnError;
+}
 
-describe('yerf()', function () {
-  describe('when a fullKey is passed in', function () {
-    it('returns the sample if it exists', function () {
-      var sample = new (yerf().Sample)('test');
-      expect(yerf('test')).toBe(sample);
+describe('yerf()', function () {  
+  beforeEach(function () {
+    kivi.clear();
+    yerf().clear(); 
+  });
+
+  describe('yerf()', function () {
+    describe('when a fullKey is passed in', function () {
+      it('returns the sample if it exists', function () {
+        var sample = new (yerf().Sample)('test');
+        expect(yerf('test')).toBe(sample);
+      });
+
+      it('returns undefined if it does not exist', function () {
+        var sample = new (yerf().Sample)('test');
+        expect(yerf('not_test')).toBe(undefined);
+      });
     });
 
-    it('returns undefined if it does not exist', function () {
-      var sample = new (yerf().Sample)('test');
-      expect(yerf('not_test')).toBe(undefined);
+    describe('when a fullKey is not passed in', function () {
+      it('returns the yerf object', function () {
+        var obj = yerf();
+        expect(typeof obj).toBe('object');
+        expect(typeof obj.create).toBe('function');
+        expect(typeof obj.start).toBe('function');
+        expect(typeof obj.Sample).toBe('function');
+      });
     });
   });
 
-  describe('when a fullKey is not passed in', function () {
-    it('returns the yerf object', function () {
-      var obj = yerf();
-      expect(typeof obj).toBe('object');
-      expect(typeof obj.create).toBe('function');
-      expect(typeof obj.start).toBe('function');
-      expect(typeof obj.Sample).toBe('function');
-    });
-  });
-});
+  describe('find()', function () {
+    describe('when a fullKey is passed in', function () {
+      it('returns the sample if it exists', function () {
+        var sample = new (yerf().Sample)('test');
+        expect(yerf().find('test')).toBe(sample);
+      });
 
-describe('find()', function () {
-  describe('when a fullKey is passed in', function () {
-    it('returns the sample if it exists', function () {
-      var sample = new (yerf().Sample)('test');
-      expect(yerf().find('test')).toBe(sample);
-    });
-
-    it('returns undefined if it does not exist', function () {
-      var sample = new (yerf().Sample)('test');
-      expect(yerf().find('not_test')).toBe(undefined);
-    });
-  });
-});
-
-describe('create()', function () {
-  describe('when a fullKey is passed in', function () {
-    it('calls onError() if the sample already exists', function () {
-      var sample = new (yerf().Sample)('test');
-      var sampleOnError = expectOnError(yerf().Sample.prototype, 'Sample[test] already exists.');
-
-      expect(yerf().create('test')).toBe(sample);
-      expect(sampleOnError).toHaveBeenCalled();
-    });
-
-    it('returns a new sample if the sample does not exist', function () {
-      var sample = yerf().create('test');
-      expect(sample.state).toBe('created');
+      it('returns undefined if it does not exist', function () {
+        var sample = new (yerf().Sample)('test');
+        expect(yerf().find('not_test')).toBe(undefined);
+      });
     });
   });
 
-  describe('when a fullKey is not passed in', function () {
-    it('throws an exception', function () {
-      expect(function() { yerf().create(); }).toThrow('You must specify a key for this Sample.');
+  describe('create()', function () {
+    describe('when a fullKey is passed in', function () {
+      it('calls onError() if the sample already exists', function () {
+        var sample = new (yerf().Sample)('test');
+        var sampleOnError = expectOnError(yerf().Sample.prototype, 'Sample[test] already exists.');
+
+        expect(yerf().create('test')).toBe(sample);
+        expect(sampleOnError).toHaveBeenCalled();
+      });
+
+      it('returns a new sample if the sample does not exist', function () {
+        var sample = yerf().create('test');
+        expect(sample.state).toBe('created');
+      });
+    });
+
+    describe('when a fullKey is not passed in', function () {
+      it('throws an exception', function () {
+        expect(function() { yerf().create(); }).toThrow('You must specify a key for this Sample.');
+      });
     });
   });
-});
 
-describe('start()', function () {
-  describe('when a fullKey is passed in', function () {
-    it('calls onError() if the sample already exists', function () {
-      var sample = new (yerf().Sample)('test');
+  describe('start()', function () {
+    describe('when a fullKey is passed in', function () {
+      it('calls onError() if the sample already exists', function () {
+        var sample = new (yerf().Sample)('test');
 
-      var sampleOnError = expectOnError(yerf().Sample.prototype, 'Sample[test] already exists.');
-      expect(yerf().start('test')).toBe(sample);
-      expect(sampleOnError).toHaveBeenCalled();
+        var sampleOnError = expectOnError(yerf().Sample.prototype, 'Sample[test] already exists.');
+        expect(yerf().start('test')).toBe(sample);
+        expect(sampleOnError).toHaveBeenCalled();
+      });
+
+      it('returns a new sample if the sample does not exist and starts it', function () {
+        var sample = yerf().start('test');
+        expect(sample.state).toBe('started');
+      });
     });
 
-    it('returns a new sample if the sample does not exist and starts it', function () {
-      var sample = yerf().start('test');
-      expect(sample.state).toBe('started');
+    describe('when a fullKey is not passed in', function () {
+      it('throws an exception', function () {
+        expect(function() { yerf().start(); }).toThrow('You must specify a key for this Sample.');
+      });
     });
   });
 
-  describe('when a fullKey is not passed in', function () {
-    it('throws an exception', function () {
-      expect(function() { yerf().start(); }).toThrow('You must specify a key for this Sample.');
+  describe('clear()', function () {
+    it('completely resets the state of yerf() and forgets any measurements taken', function () {
+     
+      yerf().start('test');
+      yerf('test').stop();
+
+      var spy = jasmine.createSpy();
+      yerf().on('key', 'event', spy);
+
+      yerf().clear();
+
+      expect(yerf('test')).toBe(undefined);
+
+      yerf().trigger('key', 'event', {});
+
+      expect(spy).not.toHaveBeenCalled();
     });
   });
-});
 
-describe('clear()', function () {
-  it('completely resets the state of yerf() and forgets any measurements taken', function () {
-   
-    yerf().start('test');
-    yerf('test').stop();
+  describe('Tests that change the epoch', function () {
+    var oldUsesModernPerf;
+    var oldModernBoot;
+    var oldOldBoot;
 
-    var spy = jasmine.createSpy();
-    yerf().on('key', 'event', spy);
+    beforeEach(function () {
+      oldUsesModernPerf = yerf().usesModernPerf;
+      oldModernBoot = yerf().modernBoot;
+      oldOldBoot = yerf().oldBoot;
+    });
 
-    yerf().clear();
+    afterEach(function () {
+      yerf().usesModernPerf = oldUsesModernPerf;
+      yerf().modernBoot = oldModernBoot;
+      yerf().oldBoot = oldOldBoot;
+    });
 
-    expect(yerf('test')).toBe(undefined);
+    describe('getTime()', function () {
+      describe('when using modern perf', function () {
+        it('returns the time since navigation start', function () {
+          yerf().usesModernPerf = true;
+          mockNow(100);
+          
+          expect(yerf().getTime()).toBe(100);
+          expect(nowSpy.calls.length).toBe(1);
+        });
+      });
 
-    yerf().trigger('key', 'event', {});
+      describe('when using old perf', function () {
+        it('returns the time since navigation start', function () {
+          yerf().usesModernPerf = false;
+          yerf().oldBoot = 100;
+          mockDate(300);
+            
+          expect(yerf().getTime()).toBe(200);
+          expect(dateSpy.calls.length).toBe(1);
+        });
+      });
+    });
 
-    expect(spy).not.toHaveBeenCalled();
+    describe('normTime()', function () {
+      describe('when using modern perf', function () {
+        it('converts unix time to yerf time', function () {
+          yerf().usesModernPerf = true;
+          yerf().modernBoot = 100;
+          yerf().oldBoot = 1000000;
+
+          expect(yerf().normTime(1000200)).toBe(100);
+        });
+      });
+
+      describe('when using old perf', function () {
+        it('converts unix time to yerf time', function () {
+          yerf().usesModernPerf = false;
+          yerf().oldBoot = 1000000;
+          
+          expect(yerf().normTime(1000200)).toBe(200);
+        });
+      });
+    });
   });
 });
